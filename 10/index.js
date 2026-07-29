283500314314ebbfa376a7b7f605fc79a765a8f5
Katia Mesquita <katiamesquita@hotmail.com>
2026-07-29 11:06:08 -0300

    ainda em andamento

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,18 @@
-const caractere = "E";
+const caractere = "1";
+
+
+if (caractere === "A" || caractere === "E" || caractere === "I" || caractere === "O" || caractere === "U") 
+    { console.log ("Vogal maíscula")}
+
+else if (caractere === "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u") 
+    { console.log ("Vogal minúscula")}
+
+else if (caractere === "1" || caractere === "2" || caractere === "3" || caractere === "4" || caractere === "5" || caractere === "6" || caractere === "7" || caractere === "8" || caractere === "9" || caractere === "0") 
+    { console.log ("Número")}
+
+else {
+    console.log ("Consoante")
+}
+
+
+

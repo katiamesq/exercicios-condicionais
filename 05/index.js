f0e88bb0fb8d9a8fc3d95e9518c4c5df02606c58
Katia Mesquita <katiamesquita@hotmail.com>
2026-08-01 11:13:02 -0300

    Operador ternário

--- 05/index.js
+++ 05/index.js
@@ -1,9 +1,15 @@
-const alturaEmCm = 185;
+//const alturaEmCm = 185;
 
 //seu código aqui
-if (alturaEmCm >= 180) {
-    console.log ("***Aprovado***")
-}
-else {
-    console.log ("Reprovado")
-}
+//if (alturaEmCm >= 180) {
+    //console.log ("***Aprovado***")
+//}
+//else {
+    //console.log ("Reprovado")
+//}
+
+
+let alturaEmCm = 185;
+let resultado = alturaEmCm >= 180 ? "aprovado" : "reprovado"
+
+console.log (resultado)
--- 13/index.js
+++ 13/index.js
@@ -4,3 +4,19 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if (tipoDePagamento === "credito")  {
+    console.log ("5%")
+}
+else if (tipoDePagamento === "cheque") {
+    console.log ("3%")
+}
+else { console.log ("10%")}
+
+//valorFinal = valorDoProduto - (valorDoProduto \* desconto)
+
+
+
+
+
+
+
